7682c92813a6364b5f0041402d3ce647e5f7fb86
ANSAR HUSEN <ansar.husen-ug@aau.edu.et>
2025-04-19 18:13:36 +0300

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
-
 import "./App.css";
+import Hero from "./sections/Hero";
 
 function App() {
   return (
-    <main className="b">
-      <div>
-        <h1 className=" ">Welcome to my 3d portfolio</h1>
-      </div>
-    </main>
+    <>
+    <Hero/>
+    </>
   );
 }
 
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+        <section id='hero' className='relative overflow-hidden'>
+            <div className='absolute left-0 top-0 z-10'>
+                <img src="images/bg.png" alt="" />
+
+            </div>
+
+        </section>
+    </div>
+  )
+}
+
+export default Hero
+
