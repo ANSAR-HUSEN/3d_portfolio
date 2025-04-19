c8d75aad721110a6624f3b08b7775d38ddd7836a
ANSAR HUSEN <ansar.husen-ug@aau.edu.et>
2025-04-19 13:52:45 +0300

    dependencies downloaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
+import "./App.css";
 
 function App() {
-
-
   return (
-    <main className='bg-gray-900'>
+    <main className="b">
       <div>
-      <h1 className='text-3xl '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eos amet aliquid ab blanditiis natus, dignissimos vel eveniet doloribus excepturi enim magni temporibus, distinctio inventore laborum atque maiores quos recusandae!</h1>
+        <h1 className=" ">Welcome to my 3d portfolio</h1>
       </div>
-      
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
